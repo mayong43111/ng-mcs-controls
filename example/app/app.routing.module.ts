9a2b753fe92ba9f5fbb8497badb571345d69b148
Yong Ma <yongma@microsoft.com>
2019-03-19 16:12:28 +0800

    add route

--- example/app/app.routing.module.ts
+++ example/app/app.routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-    // { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-    { path: '', component: WelcomeComponent, data: { pageTitle: 'We welcome you.' } },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: 'welcome', component: WelcomeComponent, data: { pageTitle: 'We welcome you.' } },
     { path: 'chat', component: WelcomeComponent, data: { pageTitle: 'We welcome you.' } },
 ];
 
